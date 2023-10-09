5a4c2750e7c1970a2ac1059f1f04629c52836b57
Kowaak <102755037+Kowaak@users.noreply.github.com>
2023-10-09 13:02:56 +0200

    Add files via upload

---
+++ przyklad1.java
@@ -0,0 +1,9 @@
+public class przyklad1 {
+    public static void main(String[] args) {
+        int num;
+        num = 200;
+        System.out.println("Wartość num wynosi "+num);
+        num = num*num;
+        System.out.println("Wartość num wynosi "+num);
+    }
+}
