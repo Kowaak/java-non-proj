e833164ffca2964a35b38550fb30bb7100f8cf21
Kowaak <102755037+Kowaak@users.noreply.github.com>
2023-11-09 08:35:44 +0100

    Delete zadanie/przyklad1.java

--- zadanie/przyklad1.java
+++
@@ -1,9 +0,0 @@
-public class przyklad1 {
-    public static void main(String[] args) {
-        int num;
-        num = 200;
-        System.out.println("Wartość num wynosi "+num);
-        num = num*num;
-        System.out.println("Wartość num wynosi "+num);
-    }
-}
