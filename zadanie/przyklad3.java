c36593fd644f24cd41def0867b77a2c5c2fccacf
Kowaak <102755037+Kowaak@users.noreply.github.com>
2023-11-09 08:35:59 +0100

    Delete zadanie/przyklad3.java

--- zadanie/przyklad3.java
+++
@@ -1,8 +0,0 @@
-public class przyklad3 {
-    public static void main(String[] args) {
-        double a = 3.0, b = 7.0;
-        // c - inicjalizacja dynamiczna
-        double c = Math.sqrt(a*a+b*b);
-        System.out.println(c);
-    }
-}
