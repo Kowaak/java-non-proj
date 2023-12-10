a69634b4895212feae5c189314ef9c302d4d32a2
Kowaak <zalecki.filip@gmail.com>
2023-12-10 13:00:37 +0100

    Wyjątki
    Ogarnąć grida jeszcze trzeba

--- Graficzne/kalkulatorGUI.java
+++ Graficzne/kalkulatorGUI.java
@@ -14,7 +14,6 @@ class calculator extends JFrame implements ActionListener {
 	public static void main(String args[])
 	{
 		f = new JFrame("calculator");
-
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		}
@@ -22,11 +21,8 @@ class calculator extends JFrame implements ActionListener {
 			System.err.println(e.getMessage());
 		}
 		calculator c = new calculator();
-
 		poletxt = new JTextField(16);
-
 		poletxt.setEditable(false);
-
 		JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, ba, bs, bd, bm, be, beq, beq1;
 		b0 = new JButton("0");
 		b1 = new JButton("1");
@@ -62,7 +58,7 @@ class calculator extends JFrame implements ActionListener {
 		be.addActionListener(c);
 		beq.addActionListener(c);
 		beq1.addActionListener(c);
-		JPanel p = new JPanel(new GridLayout(5, 3, 5, 5));
+		JPanel p = new JPanel(new GridLayout(5,3,5,5));
         p.setBackground(Color.darkGray);
 		p.add(poletxt);
 		p.add(ba);
@@ -94,79 +90,65 @@ class calculator extends JFrame implements ActionListener {
 		String s = e.getActionCommand();
 
 		if ((s.charAt(0) >= '0' && s.charAt(0) <= '9') || s.charAt(0) == '.') {
-			// if operand is present then add to second no
 			if (!s1.equals(""))
 				s2 = s2 + s;
 			else
 				s0 = s0 + s;
-
-			// set the value of text
-			poletxt.setText(s0 + s1 + s2);
+        if(s0.contains(".") && s.contains("."))
+        {
+            JOptionPane.showMessageDialog(f, "Błąd: Liczba może zawierać tylko jedną kropkę", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+		poletxt.setText(s0 + s1 + s2);
 		}
 		else if (s.charAt(0) == 'C') {
-			// clear the one letter
 			s0 = s1 = s2 = "";
-
-			// set the value of text
 			poletxt.setText(s0 + s1 + s2);
 		}
 		else if (s.charAt(0) == '=') {
-
 			double te;
-
-			// store the value in 1st
 			if (s1.equals("+"))
 				te = (Double.parseDouble(s0) + Double.parseDouble(s2));
 			else if (s1.equals("-"))
 				te = (Double.parseDouble(s0) - Double.parseDouble(s2));
-			else if (s1.equals("/"))
-				te = (Double.parseDouble(s0) / Double.parseDouble(s2));
-			else
-				te = (Double.parseDouble(s0) * Double.parseDouble(s2));
-
-			// set the value of text
-			poletxt.setText(s0 + s1 + s2 + "=" + te);
-
-			// convert it to string
-			s0 = Double.toString(te);
-
-			s1 = s2 = "";
+				else if (s1.equals("/"))
+	    	        if(Double.parseDouble(s2) == 0){
+	    	            JOptionPane.showMessageDialog(f, "Błąd: Dzielenie przez 0", "Error", JOptionPane.ERROR_MESSAGE);
+	    	            return;
+	    	        }
+	    	        else{
+					te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+	    	        }
+				else
+					te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+				poletxt.setText(s0 + s1 + s2 + "=" + te);
+				s0 = Double.toString(te);
+				s1 = s2 = "";
 		}
 		else {
-			// if there was no operand
 			if (s1.equals("") || s2.equals(""))
 				s1 = s;
-			// else evaluate
 			else {
 				double te;
 
-				// store the value in 1st
 				if (s1.equals("+"))
 					te = (Double.parseDouble(s0) + Double.parseDouble(s2));
 				else if (s1.equals("-"))
 					te = (Double.parseDouble(s0) - Double.parseDouble(s2));
 				else if (s1.equals("/"))
-                    te = (Double.parseDouble(s0) / Double.parseDouble(s2));
-                    /*if(Double.parseDouble(s1) == 0 || Double.parseDouble(s2) == 0){
-                        te = Double.parseDouble("Błąd: Dzielenie przez 0");
-                    }
-                    else{
-					te = (Double.parseDouble(s0) / Double.parseDouble(s2));
-                    }*/
+	                if(Double.parseDouble(s2) == 0){
+	                    JOptionPane.showMessageDialog(f, "Błąd: Dzielenie przez 0", "Error", JOptionPane.ERROR_MESSAGE);
+	                    return;
+	                }
+	                else{
+						te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+	                }
 				else
 					te = (Double.parseDouble(s0) * Double.parseDouble(s2));
-
-				// convert it to string
 				s0 = Double.toString(te);
-
-				// place the operator
 				s1 = s;
-
-				// make the operand blank
 				s2 = "";
 			}
-
-			// set the value of text
 			poletxt.setText(s0 + s1 + s2);
 		}
 	}
