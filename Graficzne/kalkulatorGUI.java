17ecd14f7cd33f435bdb1ac91b060eb7155ccba2
Kowaak <102755037+Kowaak@users.noreply.github.com>
2023-12-11 13:11:41 +0100

    FAZA MANYU

--- Graficzne/kalkulatorGUI.java
+++ Graficzne/kalkulatorGUI.java
@@ -13,7 +13,7 @@ class calculator extends JFrame implements ActionListener {
 	}
 	public static void main(String args[])
 	{
-		f = new JFrame("calculator");
+		f = new JFrame("Kalkulator - Autor: Filip Załęcki");
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		}
@@ -23,6 +23,8 @@ class calculator extends JFrame implements ActionListener {
 		calculator c = new calculator();
 		poletxt = new JTextField(16);
 		poletxt.setEditable(false);
+		//Image icon = Toolkit.getDefaultToolkit().getImage("logo.png");    
+		//f.setIconImage(icon);    
 		JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, ba, bs, bd, bm, be, beq, beq1;
 		b0 = new JButton("0");
 		b1 = new JButton("1");
@@ -58,9 +60,10 @@ class calculator extends JFrame implements ActionListener {
 		be.addActionListener(c);
 		beq.addActionListener(c);
 		beq1.addActionListener(c);
-		JPanel p = new JPanel(new GridLayout(5,3,5,5));
+		JPanel p = new JPanel(new GridLayout(5,3,5,5)); 
         p.setBackground(Color.darkGray);
 		p.add(poletxt);
+		p.add(beq);
 		p.add(ba);
 		p.add(b1);
 		p.add(b2);
@@ -76,7 +79,6 @@ class calculator extends JFrame implements ActionListener {
 		p.add(bd);
 		p.add(be);
 		p.add(b0);
-		p.add(beq);
 		p.add(beq1);
 
 		p.setBackground(Color.darkGray);
@@ -96,7 +98,7 @@ class calculator extends JFrame implements ActionListener {
 				s0 = s0 + s;
         if(s0.contains(".") && s.contains("."))
         {
-            JOptionPane.showMessageDialog(f, "Błąd: Liczba może zawierać tylko jedną kropkę", "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(f, "Błąd: Liczba może zawierać tylko jedną kropkę", "Błąd", JOptionPane.ERROR_MESSAGE);
             return;
         }
 		poletxt.setText(s0 + s1 + s2);
@@ -113,7 +115,7 @@ class calculator extends JFrame implements ActionListener {
 				te = (Double.parseDouble(s0) - Double.parseDouble(s2));
 				else if (s1.equals("/"))
 	    	        if(Double.parseDouble(s2) == 0){
-	    	            JOptionPane.showMessageDialog(f, "Błąd: Dzielenie przez 0", "Error", JOptionPane.ERROR_MESSAGE);
+	    	            JOptionPane.showMessageDialog(f, "Błąd: Dzielenie przez 0", "Błąd", JOptionPane.ERROR_MESSAGE);
 	    	            return;
 	    	        }
 	    	        else{
@@ -124,12 +126,12 @@ class calculator extends JFrame implements ActionListener {
 				poletxt.setText(s0 + s1 + s2 + "=" + te);
 				s0 = Double.toString(te);
 				s1 = s2 = "";
-		}
-		else {
-			if (s1.equals("") || s2.equals(""))
-				s1 = s;
-			else {
-				double te;
+				}
+				else {
+					if (s1.equals("") || s2.equals(""))
+					s1 = s;
+				else {
+					double te;
 
 				if (s1.equals("+"))
 					te = (Double.parseDouble(s0) + Double.parseDouble(s2));
